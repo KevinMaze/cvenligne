f7a6b33bb49d5b8d969fa4bb617d82000813b4a1
Kévin MAZE <kekeproject@hotmail.fr>
2024-07-20 19:21:14 +0200

    add file style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,16 @@ import Nav from '../components/Nav';
 
 const Home = () => {
     return (
-        <>
-            <div>
+            <div className='home'>
                 <Nav />
+                <div className="homeContent">
+                    <h1>MAZE Kévin</h1>
+                    <h2>Développeur front-end</h2>
+                    <div className="pdf">
+                        <a href="*" target='_blank'>Télécharger le CV</a>
+                    </div>
+                </div>
             </div>
-            <div>
-                Wesh ma gueule
-                
-            </div>
-        </>
     );
 };
 
